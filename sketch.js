ab57bc98f036e01305b93dc090e6e2858aaadf8f
zaldeth <113926156+zaldeth@users.noreply.github.com>
2022-12-26 19:14:42 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -75,51 +75,40 @@ function draw() {
   playerArcher.display();
   computerArcher.display()
 
- 
- for (var i=0; i<playerArrows; i++) {
+ // Uncomment and use correct for loop to display arrow using showArrow() function
+ for (var i=0; i<playerArrows.length; i++) 
+ {
  showArrows(i, playerArrows);
- showArrows(balls[i], i);
-
  }
-
-
-
-
-
 
 }
 
-/*********** Choose correct keyPressed() function out of these *************/
+
 
 function keyPressed() {
   if(keyCode === 32){
-     //create an arrow object and add into an array ; set its angle same as angle of playerArcher
+    // create an arrow object and add into an array ; set its angle same as angle of playerArcher
     var posX = playerArcher.body.position.x;
     var posY = playerArcher.body.position.y;
-    var angle = playerArcher.body
+    var angle = playerArcher.body.angle+PI/2;
     var arrow = new PlayerArrow(posX, posY, 100, 10);
     arrow.trajectory = [];
-    Matter.Body.setAngle(arrow.body);
+    Matter.Body.setAngle(arrow.body, angle);
     playerArrows.push(arrow);
   }
 }
 
-
-
-
-
-
-//function keyReleased () {
+function keyReleased () {
 
   if(keyCode === 32){
     //call shoot() function for each arrow in an array playerArrows
-    if (playerArrows.length) { 
+    if (playerArrows.length) {
       var angle = playerArcher.body.angle+PI/2;
       playerArrows[playerArrows.length - 1].shoot(angle);
     }
   }
 
-//}
+}
 //Display arrow and Tranjectory
 function showArrows(index, arrows) {
   arrows[index].display();
